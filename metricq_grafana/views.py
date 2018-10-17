07c1892675e2148cf0e20927ae3aa92bb03993d9
Franz Schmidt <franz.schmidt2@tu-dresden.de>
2018-10-17 10:55:10 +0200

    Replace print with logger.debug

--- metricq_grafana/views.py
+++ metricq_grafana/views.py
@@ -1,13 +1,16 @@
 """Module for view functions"""
 from aiohttp import web
 
+from metricq import get_logger
+
 from .amqp import get_history_data, get_metric_list
-#from .constants import METRIC_LIST
+
+logger = get_logger(__name__)
 
 
 async def query(request):
     req_json = await request.json()
-    print(req_json)
+    logger.debug("Query request data: {}", req_json)
     resp = await get_history_data(request.app, req_json)
     return web.json_response(
         resp
@@ -17,7 +20,7 @@ async def query(request):
 async def search(request):
     search_query = (await request.json())["target"]
     metric_list = await get_metric_list(request.app)
-    print(search_query)
+    logger.debug("Search query: {}", search_query)
     if search_query == "":
         ml = metric_list
     else:
