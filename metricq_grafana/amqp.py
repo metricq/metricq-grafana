9c91a2a07abc790808f28a826d2173c394423fbd
Franz Schmidt <valsatize@gmail.com>
2019-04-18 15:10:08 +0200

    Implement alias functions (#8), group requests
    * Implements Target class to group all string parsing for targets, also alias support
    * Pull description for alias from metricq
    * Refactor data gathering in target class

--- metricq_grafana/amqp.py
+++ metricq_grafana/amqp.py
@@ -1,79 +1,19 @@
 import asyncio
-import calendar
-import datetime
-import json
+import functools
 import re
-from statistics import mean
-import time
-import uuid
-import math
-
-import aio_pika
+import operator
 
 from metricq import get_logger
+from .utils import Target
 
 logger = get_logger(__name__)
 
 
-def sanitize_number(value):
-    """ Convert NaN and Inf to None - because JSON is dumb """
-    if math.isfinite(value):
-        return value
-    return None
-
-
 async def get_history_data(app, request):
-    targets = [x["target"] for x in request["targets"]]
-    results = []
+    targets = [Target.extract_from_string(x["target"]) for x in request["targets"]]
+    results = await asyncio.gather(*[target.get_response(app, request) for target in targets])
 
-    for target in targets:
-        target_split = target.split("/")
-        if len(target_split) > 1:
-            target_metric = "/".join(target_split[:-1])
-            target_types = [target_split[-1]]
-        else:
-            target_metric = target
-            target_types = ["avg"]
-        template_var_match = re.fullmatch(r"\((?P<multitype>((min|max|avg)\|?)+)\)", target_types[0])
-        if template_var_match:
-            target_types = template_var_match.group("multitype").split("|")
-        start_time = int(datetime.datetime.strptime(request["range"]["from"], "%Y-%m-%dT%H:%M:%S.%fZ").replace(tzinfo=datetime.timezone.utc).timestamp() * (10 ** 9))
-        end_time = int(datetime.datetime.strptime(request["range"]["to"], "%Y-%m-%dT%H:%M:%S.%fZ").replace(tzinfo=datetime.timezone.utc).timestamp() * (10 ** 9))
-        interval_ns = request["intervalMs"] * 10 ** 6
-        perf_start_time = time.perf_counter_ns()
-        rep = await app['history_client'].history_data_request(target_metric, start_time, end_time, interval_ns)
-        perf_end_time = time.perf_counter_ns()
-        time_delta_ns = (perf_end_time - perf_start_time)
-        time_delta = time_delta_ns / (10 ** 9)
-        app['last_perf_list'].insert(0, time_delta)
-        if 'last_perf_log' not in app or app['last_perf_log'] < datetime.datetime.now() - datetime.timedelta(seconds=10):
-            app['last_perf_list'] = app['last_perf_list'][:100]
-            max_value = datetime.timedelta(seconds=max(app['last_perf_list']))
-            min_value = datetime.timedelta(seconds=min(app['last_perf_list']))
-            avg_value = datetime.timedelta(seconds=mean(app['last_perf_list']))
-            logger.info('Last 100 metricq data reponse times: min {}, max {}, avg {}', min_value, max_value, avg_value)
-            app['last_perf_log'] = datetime.datetime.now()
-
-        for target_type in target_types:
-            rep_dict = {"target": "{}/{}".format(target_metric, target_type), "datapoints": [], "time_measurements": {"db": rep.request_duration, "http": str(time_delta_ns)} }
-            last_timed = 0
-
-            if target_type == "min":
-                zipped_tv = zip(rep.time_delta, rep.value_min)
-            elif target_type == "max":
-                zipped_tv = zip(rep.time_delta, rep.value_max)
-            else:
-                zipped_tv = zip(rep.time_delta, rep.value_avg)
-
-            for timed, value in zipped_tv:
-                dp = rep_dict["datapoints"]
-                last_timed += timed
-                dp.append((sanitize_number(value), (last_timed / (10 ** 6))))
-                rep_dict["datapoints"] = dp
-
-            results.append(rep_dict)
-
-    return results
+    return functools.reduce(operator.iconcat, results, [])
 
 
 async def get_metric_list(app, search_query):
---
+++ metricq_grafana/utils.py
@@ -0,0 +1,148 @@
+import asyncio
+import datetime
+import math
+import re
+import time
+
+
+def sanitize_number(value):
+    """ Convert NaN and Inf to None - because JSON is dumb """
+    if math.isfinite(value):
+        return value
+    return None
+
+
+class Target:
+
+    # Target types
+    ALIAS = "alias"
+    METRIC = "metric"
+    DESC = "description"
+    METRICDESC = "metricAndDescription"
+
+    def __init__(self):
+        self.target = ""
+        self.alias_type = None
+        self.alias_value = ""
+        self.aggregation_types = ["avg"]
+        self.response = None
+        self.time_delta_ns = None
+
+    @classmethod
+    def extract_from_string(cls, target_string: str):
+        target = cls()
+        target_string = target._extract_alias(target_string)
+        target_split = target_string.split("/")
+        if len(target_split) > 1:
+            target.target = "/".join(target_split[:-1])
+            target.aggregation_types = [target_split[-1]]
+        else:
+            target.target = target_string
+        template_var_match = re.fullmatch(r"\((?P<multitype>((min|max|avg)\|?)+)\)", target.aggregation_types[0])
+        if template_var_match:
+            target.aggregation_types = template_var_match.group("multitype").split("|")
+        return target
+
+    def _extract_alias(self, target_string) -> str:
+        extracted_target_string = target_string
+        prefix_length = None
+        suffix_length = 0
+        if target_string.startswith("alias("):
+            self.alias_type = Target.ALIAS
+            self.alias_value = ",".join(target_string.split(",")[1:])[:-1]
+            prefix_length = len("alias(")
+            suffix_length = len(self.alias_value) + 2  # remove alias string, closing bracket and separation comma
+        elif target_string.startswith("aliasByMetric("):
+            self.alias_type = Target.METRIC
+            prefix_length = len("aliasByMetric(")
+            suffix_length = 1
+        elif target_string.startswith("aliasByDescription("):
+            self.alias_type = Target.DESC
+            self.alias_value = "No description found"
+            prefix_length = len("aliasByDescription(")
+            suffix_length = 1
+        elif target_string.startswith("aliasByMetricAndDescription("):
+            self.alias_type = Target.METRICDESC
+            self.alias_value = "No description found"
+            prefix_length = len("aliasByMetricAndDescription(")
+            suffix_length = 1
+        if prefix_length:
+            extracted_target_string = target_string[prefix_length:-suffix_length]
+        return extracted_target_string
+
+    def get_target_as_regex(self):
+        return "^{}$".format(re.escape(self.target))
+
+    def convert_response(self, response, time_measurement):
+        results = []
+        for target_type in self.aggregation_types:
+            rep_dict = {
+                "target": (self.get_aliased_target(aggregation_type=target_type)),
+                "datapoints": [],
+                "time_measurements": {"db": response.request_duration, "http": str(time_measurement)}
+            }
+            last_timed = 0
+
+            if target_type == "min":
+                zipped_tv = zip(response.time_delta, response.value_min)
+            elif target_type == "max":
+                zipped_tv = zip(response.time_delta, response.value_max)
+            else:
+                zipped_tv = zip(response.time_delta, response.value_avg)
+
+            for timed, value in zipped_tv:
+                dp = rep_dict["datapoints"]
+                last_timed += timed
+                dp.append((sanitize_number(value), (last_timed / (10 ** 6))))
+                rep_dict["datapoints"] = dp
+
+            results.append(rep_dict)
+
+        return results
+
+    def get_aliased_target(self, aggregation_type=None) -> str:
+        if not self.alias_type:
+            if aggregation_type:
+                return "{}/{}".format(self.target, aggregation_type)
+            else:
+                return self.target
+        if self.alias_type == Target.ALIAS or self.alias_type == Target.DESC:
+            return self.alias_value
+        elif self.alias_type == Target.METRIC:
+            if aggregation_type:
+                return "{}/{}".format(self.target.replace(".", "/"), aggregation_type)
+            else:
+                return self.target.replace(".", "/")
+        elif self.alias_type == Target.METRICDESC:
+            if aggregation_type:
+                return "{}/{} ({})".format(self.target.replace(".", "/"), aggregation_type, self.alias_value)
+            else:
+                return "{} ({})".format(self.target.replace(".", "/"), self.alias_value)
+
+    async def pull_description(self, app):
+        if self.alias_type not in (Target.DESC, Target.METRICDESC):
+            return
+        metadata = await app["history_client"].history_metric_metadata(selector=self.get_target_as_regex())
+        if self.target not in metadata:
+            return
+        if "description" in metadata[self.target]:
+            self.alias_value = metadata[self.target]["description"]
+        return
+
+    async def pull_data(self, app, request):
+        start_time = int(datetime.datetime.strptime(request["range"]["from"], "%Y-%m-%dT%H:%M:%S.%fZ").replace(
+            tzinfo=datetime.timezone.utc).timestamp() * (10 ** 9))
+        end_time = int(datetime.datetime.strptime(request["range"]["to"], "%Y-%m-%dT%H:%M:%S.%fZ").replace(
+            tzinfo=datetime.timezone.utc).timestamp() * (10 ** 9))
+        interval_ns = request["intervalMs"] * 10 ** 6
+        perf_start_time = time.perf_counter_ns()
+        self.response = await app['history_client'].history_data_request(self.target, start_time, end_time, interval_ns)
+        perf_end_time = time.perf_counter_ns()
+        self.time_delta_ns = (perf_end_time - perf_start_time)
+
+    async def get_response(self, app, request):
+        await asyncio.wait([self.pull_data(app, request), self.pull_description(app)])
+
+        if self.response is None or self.time_delta_ns is None:
+            return []
+        return self.convert_response(self.response, self.time_delta_ns)
