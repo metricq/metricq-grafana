f0369db6d7de288f920e2011c5947d4e9c0c0b0d
tilsche <thomas.ilsche@tu-dresden.de>
2019-07-11 16:14:10 +0200

    cache get_metrics

--- metricq_grafana/client.py
+++ metricq_grafana/client.py
@@ -1,5 +1,6 @@
-from aiocache import SimpleMemoryCache, cached
+from typing import Optional, Sequence, Union
 
+from aiocache import SimpleMemoryCache, cached
 from metricq import HistoryClient
 
 
@@ -7,3 +8,15 @@ class Client(HistoryClient):
     @cached(ttl=10 * 60, cache=SimpleMemoryCache, noself=True)
     async def history_metric_metadata(self, selector=None):
         return await super().history_metric_metadata(selector=selector)
+
+    @cached(ttl=10 * 60, cache=SimpleMemoryCache, noself=True)
+    async def get_metrics(
+        self,
+        selector: Union[str, Sequence[str], None] = None,
+        metadata: bool = True,
+        historic: Optional[bool] = None,
+        timeout: Optional[float] = None,
+    ) -> Union[Sequence[str], Sequence[dict]]:
+        return await super().get_metrics(
+            selector=selector, metadata=metadata, historic=historic, timeout=timeout
+        )
